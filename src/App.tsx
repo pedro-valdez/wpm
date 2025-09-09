373815cb4dfe51e6e3d8fbd73f2b18fd5151eb4a
Pedro Valdez <79939920+pedro-valdez@users.noreply.github.com>
2025-09-09 11:28:07 -0500

    feat(mvp): Add user input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { atom, useAtom, useAtomValue } from 'jotai'
 import { generate } from 'random-words'
 
+const promptAtom = atom(generate(100) as string[])
+const userInputAtom = atom('')
+
 function App() {
-  const prompt = generate(100) as string[]
+  const prompt = useAtomValue(promptAtom)
+  const [userInput, setUserInput] = useAtom(userInputAtom)
 
   return (
-    <p className="w-full max-w-full space-x-[1ch]">
-      {prompt.map((word, i) => (
-        <span key={i} className="inline-block">
-          {word}
-        </span>
-      ))}
-    </p>
+    <>
+      <p className="w-full max-w-full space-x-[1ch]">
+        {prompt.map((word, i) => (
+          <span key={i} className="inline-block">
+            {word}
+          </span>
+        ))}
+      </p>
+
+      <input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+    </>
   )
 }
 
