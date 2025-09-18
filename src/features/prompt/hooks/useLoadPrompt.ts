882ff9289be50a6ce09c35cfaa311bc4654caf5f
pedro-valdez <79939920+pedro-valdez@users.noreply.github.com>
2025-09-18 15:47:14 -0500

    feat(capitalization): Add capitalization setting
    * feat(capitalization): Add capitalization logic for prompt
    
    * feat(capitalization): Add capitalization toggle component
    
    * feat(capitalization): Add control flow of capitalization when punctuation is true

--- src/features/prompt/hooks/useLoadPrompt.ts
+++ src/features/prompt/hooks/useLoadPrompt.ts
@@ -1,14 +1,19 @@
 import { useCallback } from 'react'
-import { promptAtom, punctuationAtom } from '../atoms'
+import { capitalizationAtom, promptAtom, punctuationAtom } from '../atoms'
 import { useSetAtom } from 'jotai'
 import { generate } from 'random-words'
 import { useAtomValue } from 'jotai'
+import { capitalize, some } from 'lodash'
 
-const PUNCTUATIONS = ['.', ',', ';', '!', '?'] as const
+const CAPITALIZE_PUNCTUATIONS = ['.', '!', '?'] as const
+const PUNCTUATIONS = [...CAPITALIZE_PUNCTUATIONS, ',', ';'] as const
 const PUNCTUATION_DENSITY = 0.2
+
+const CAPITALIZATION_DENSITY = 0.2
 
 export function useLoadPrompt() {
   const punctuation = useAtomValue(punctuationAtom)
+  const capitalization = useAtomValue(capitalizationAtom)
   const setPrompt = useSetAtom(promptAtom)
 
   const loadPrompt = useCallback(() => {
@@ -23,8 +28,27 @@ export function useLoadPrompt() {
       })
     }
 
+    if (capitalization && punctuation) {
+      words = words.map((word, index) => {
+        if (index === 0) {
+          return capitalize(word)
+        }
+        // NOTE: The check is not necessary, but just in case...
+        const previousWord = index === 0 ? '' : words[index - 1]
+        const previousIsPunctuation = some(CAPITALIZE_PUNCTUATIONS, (punc) =>
+          previousWord.endsWith(punc)
+        )
+
+        return previousIsPunctuation ? capitalize(word) : word
+      })
+    } else if (capitalization) {
+      words = words.map((word) =>
+        Math.random() <= CAPITALIZATION_DENSITY ? capitalize(word) : word
+      )
+    }
+
     setPrompt(words)
-  }, [punctuation, setPrompt])
+  }, [punctuation, capitalization, setPrompt])
 
   return loadPrompt
 }
